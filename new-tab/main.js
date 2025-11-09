06e53b334e723c08cec5ff03d6e6b16294e78745
Harry Jarry <148587892+HarryTarryJarry@users.noreply.github.com>
2025-11-08 19:28:09 -0500

    The great condomnation commit

--- new-tab/main.js
+++ new-tab/main.js
@@ -8,11 +8,11 @@ import { BackgroundController } from "./background-controller.js";
 const WIFI_URL = "chrome://network/#select";
 const BLUETOOTH_URL = "chrome://bluetooth-pairing";
 const SETTINGS_URL = "chrome://settings";
-const NEW_TAB_URL = "chrome://new-tab-page";
+const CUSTOMIZE_URL = "chrome://customize-chrome-side-panel.top-chrome";
 const FILES_URL = "chrome://file-manager";
 const HELP_URL = "https://github.com/AceOfSpades1061/skiovox-helper_mv3";
 const WEBSTORE_URL = "https://chromewebstore.google.com";
-const ADDSESSION_URL = "https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAlAmgQ&flowName=GlifWebSignIn&flowEntry=AddSession";
+const ADDSESSION_URL = "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fwww.google.com%2F&hl=en&flowName=GlifWebSignIn&flowEntry=AddSession";
 
 let [
     help,
@@ -49,8 +49,7 @@ settings.addEventListener('click', () => {
 })
 
 theme.addEventListener('click', () => {
-    alert("The normal New Tab page will now open. On that page, click the pencil icon in the bottom right corner to edit your browser theme.")
-    chrome.tabs.create({ url: NEW_TAB_URL })
+    chrome.tabs.create({ url: CUSTOMIZE_URL })
 })
 
 files.addEventListener('click', () => {
